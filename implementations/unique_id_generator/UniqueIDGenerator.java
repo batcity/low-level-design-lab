f43663bb30e54871424d512f6b463a8b9a6eb271
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-06-12 14:45:08 +0530

    create an example unique ID generator in java

--- implementations/unique_id_generator/UniqueIDGenerator.java
+++ implementations/unique_id_generator/UniqueIDGenerator.java
@@ -1,3 +1,8 @@
+import java.util.UUID;
+
 public class UniqueIDGenerator {
-    
+
+    public static void main(String args[]) {
+        System.out.println("Here's a new UUID: " + UUID.randomUUID());
+    }
 }
